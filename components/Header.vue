6ca34090492ca3b72b5b8c002092eee5f984e39a
kdingn <koukou3335@gmail.com>
2024-02-11 14:57:22 +0900

    add header icon

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiGithub } from "@mdi/js";
+</script>
 
 <template>
-  <h1 class="header">Nuxt request compare...</h1>
+  <div class="header">
+    <div>
+      <h1 class="header-title">Nuxt3 request compare...</h1>
+    </div>
+    <a href="https://github.com/kdingn">
+      <svg-icon type="mdi" :path="mdiGithub" class="button-icon"></svg-icon>
+    </a>
+  </div>
   <hr class="hr" size="1" />
 </template>
 
 <style scoped>
 .header {
   margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+.header-title {
+  margin: 0 auto;
+  margin-left: 0.3rem;
+}
+.button-icon {
+  margin-top: 0.1rem;
+  margin-right: 0.1rem;
+  color: rgba(200, 198, 191);
+  zoom: 1.5;
 }
 .hr {
   margin: 0 auto;
