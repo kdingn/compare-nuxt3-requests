ebf4d479c619c76059345102c2090e321885301c
kdingn <koukou3335@gmail.com>
2024-02-11 13:34:29 +0900

    right content completed

--- components/Right.vue
+++ components/Right.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+var inputUrl = ref("");
+const data = ref<any>("");
+
+function setUrl() {
+  inputUrl.value = "https://ai-data-base.com/archives/64001";
+}
+
+function updateContent() {
+  $fetch(inputUrl.value).then((res) => {
+    data.value = res;
+  });
+}
+</script>
 
 <template>
-  <div>righttttt content</div>
+  <div class="right-content">
+    <div>client</div>
+    <div>
+      <input type="form" class="right-input" v-model="inputUrl" />
+      <button @click="updateContent">request</button>
+      <button @click="setUrl">set url</button>
+    </div>
+    <div>{{ data }}</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.right-content {
+  width: 49vw;
+}
+.right-input {
+  width: 30vw;
+}
+</style>
